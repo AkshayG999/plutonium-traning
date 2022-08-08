b7617b2ab0c38a99f1d350df0f3b0147385e6ffd
unknown <https://github.com/AkshayG999/Functionup.git>
2022-08-08 14:35:46 +0530

    Lodash Add

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,47 @@
 const express = require('express');
+const { union } = require('lodash');
 // const abc = require('../introduction/intro')
+const lodash=require('lodash')
 
 const msg= require('../logger/logger')
 const day=require('../util/helper')            // module path
 const abc= require('../validatore/formatter')
 
+
 const router = express.Router();
 router.get('/test-me', function (req, res) {
-
     // console.log('My batch is', abc.name)
     // abc.printName()
+    const array=["jan","Feb","March","April","May","Jun","July","Aug","Sep","Oct","Nov","Dec"]
+    const result=lodash.chunk(array,3)
+    console.log(result)
 
+
+const array2=[1,3,5,7,9,11,13,15,17,19]
+const result2=lodash.tail(array2)
+console.log(result2)
+ 
+const array3=[1,2,4,5,4,3,7,5]
+const result3=lodash.union(array3)
+console.log(result3)
+
+const array4=[['horror','The Shining'],['drama','Titanic'],['thriller','Shutter Island'],['fantasy','Pans Labyrinth']]
+const result4=lodash. fromPairs(array4)
+console.log(result4)
+
+    
     msg.welcome()    
     day.batch()      //Call Fuction
+    day.todayDate()
     abc.hardcode()
+
+
+
+
+
+
+
+
 
     res.send('My second ever api!')
 });
--- src/util/helper.js
+++ src/util/helper.js
@@ -1,21 +1,23 @@
+ let todayDate=function(){
+
  
 
-let ts= Date.now();
-
-let date_ob = new Date(ts);
-let date = date_ob.getDate();
-let month = date_ob.getMonth() + 1;
+let date_ob = new Date();    //new Date()-current date time month,info show karta hai
+let date = date_ob.getDate();  //hame sirf date leni hai upper se to isliye .getdate() function liya
+let month = date_ob.getMonth() + 1;  //
  
 console.log("Today Date" +" " + date + "-" + month )
- 
+
+ }
 let batch= function(){
 console.log("Plutonium,W3D5,the topic for today is Nodejs module system ")
 }
 
 
 
-module.exports.date=date
-module.exports.month=month     //private module to public module.
-module.exports.ts=ts
-module.exports.date_ob=date_ob
+// module.exports.date=date
+// module.exports.month=month     //private module to public module.
+// module.exports.ts=ts
+// module.exports.date_ob=date_ob
 module.exports.batch=batch
+module.exports.todayDate=todayDate
