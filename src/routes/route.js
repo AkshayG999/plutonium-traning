15d87d79e0c583b8798f5b5e7c1861a4f352bc10
unknown <https://github.com/AkshayG999/Functionup.git>
2022-08-10 23:28:37 +0530

    Missing NO

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const { application } = require('express');
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
-const app = express.Router();
+const app = express();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
@@ -20,22 +20,25 @@ router.get('/give-me-students-data',function(req, res){
 })
 
 
-app.get('/missingNumberArray',function(req,res){
-let array=[1,2,4,6,7,9]
+router.get('/missingNumberArray-1',function(req,res){
+let array=[1,2,3,4,6,7,8,9]
 
-//solution 1 way
-missingNumber=[]
-for (i=1 ; i<array.length ; i++){
-    if(array.indexOf(i) === -1){    //
-        missingNumber.push(i)
-        res.send ( {data:missingNumber} )
-    }
-}
+//solution 1st way
+// missingNumber=[]
+// for (let i=1 ; i<array.length ; i++){
 
-//solution 2 way
+//     if(array.indexOf(i) === -1){    //
+
+//         missingNumber.push(i)
+
+        
+//     }
+// }return res.send ( {data:missingNumber} )
+
+//solution 2nd way
 let total=0
 for(var i in array){
-total=total+arr[i]
+total=total+array[i]
 }
   
  let lastNum=array.pop()
@@ -48,17 +51,22 @@ total=total+arr[i]
 
 
 
-app.get('',function(req,res){
+router.get('/missingNumberArray-2',function(req,res){
 
-  let array=[32,34,35,37,38,39]
-   let total=0
-   for(var i in array){
-   total=total+array[i]
-   }
-     
-    let 
+    let array=[32,33,34,35,37,38,39]
+    let total=0
+    let len=array.length
+ 
+    for(var i in array){
+    total=total+array[i]
+    }
+      
+     let firstNum=array[0]
+     let lastNum=array.pop()
+     consecutivSum =  (len + 1)*(firstNum+ lastNum)/2
+     missingNumber=consecutivSum - total
 
-
+     res.send({ data:missingNumber,status:true})
 
 })
 module.exports = router;
