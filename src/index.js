dcdbe2a9466dd5b7e10f85e9c9e44efbe567bafc
unknown <akshay.ag544@gmail.com>
2022-08-17 17:50:42 +0530

    as

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect(" mongodb+srv://AkshayGaikwad:Akshay143@cluster0.ii90wme.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://AkshayGaikwad:Akshay143@cluster0.ii90wme.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
