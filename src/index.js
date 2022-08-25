69a75dd7b109b97ed0b0864463a6bbb08d35f22f
unknown <akshay.ag544@gmail.com>
2022-08-26 01:52:36 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment = require('moment')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
@@ -17,6 +19,10 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 app.use (
     function (req, res, next) {
         console.log ("inside GLOBAL MW");
+        let timstamp = moment().format('YYYY_MM_DD h:mm:ss a')
+        let ip =req.ip
+        let url=req.originalUrl
+        console.log(timstamp,ip,url)
         next();
   }
   );
